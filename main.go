0aa05cd8ac1ad2d7eb2e270279d45bb4961a51b5
Arab45 <horlakunleolayiwola@gmail.com>
2025-06-03 10:29:07 -0700

    go to improve code structure and readability; add first name extraction from bookings

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"strings"
+)
 
 
 func main() {
@@ -7,11 +11,11 @@ func main() {
 	var conferenceName string = "Go conference";
 	const conferenceTickets int = 50;
 	var remainingTickets int = 50;
-	var bookings [50]string;
+	// var bookings [50]string;
 	var slices []string;
 
-	slices = append(slices, "John Smith");
-	fmt.Println("slice in Go is also an array", slices[0]);
+	// slices = append(slices, "John Smith");
+	// fmt.Println("slice in Go is also an array", slices[0]);
 
 	fmt.Println(&conferenceName);
 	fmt.Println(&remainingTickets);
@@ -22,14 +26,16 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets);
 	fmt.Println("Get your tickets here to attend");	
 
+	for {
 	var firstName string;
 	var lastName string;
 	var email string;
 	var tickets int;
 
+
 	fmt.Println("Enter your first name: ");
 	fmt.Scan(&firstName);
-fmt.Println("Enter your last name: ");
+    fmt.Println("Enter your last name: ");
 	fmt.Scan(&lastName);
 	fmt.Println("Enter your email: ");
 	fmt.Scan(&email);
@@ -37,24 +43,38 @@ fmt.Println("Enter your last name: ");
 	fmt.Scan(&tickets);
 	fmt.Printf("User %v %v with email %v booked %v tickets\n", firstName, lastName, email, tickets);
 
-	var userName string;
-	var userTickets int;
-	fmt.Println("Enter your name: ");
-	fmt.Scan(&userName);
-	fmt.Println("Enter number of tickets: ");
-    fmt.Scan(&userTickets);
-	fmt.Println(&userName);
-	fmt.Println(&userTickets);
-	if userTickets > 0{
-		var leftTickets int = remainingTickets - userTickets;
-		fmt.Printf("Booking tickets left to purchase: %v\n", leftTickets); 
-	};
+	// bookings[0] = firstName + " " + lastName;
+	// fmt.Printf("List of bookings is %v\n", bookings);
+	// fmt.Printf("Type of bookings is %T\n", bookings);
+	// fmt.Printf("First booking is %v\n", bookings[0]);
+	// fmt.Printf("Number of bookings is %v\n", len(bookings));
+	slices = append(slices, firstName + " " + lastName);
 
-	bookings[0] = firstName + " " + lastName;
-	fmt.Printf("List of bookings is %v\n", bookings);
-	fmt.Printf("Type of bookings is %T\n", bookings);
-	fmt.Printf("First booking is %v\n", bookings[0]);
-	fmt.Printf("Number of bookings is %v\n", len(bookings));
+	firstNames := []string{};
+	for _, values := range slices {
+		var names = strings.Fields(values);
+		firstNames = append(firstNames, names[0]);
+	}
 
-	fmt.Printf("User %v booked %v tickets\n", userName, userTickets);
+	fmt.Printf("The first name of bookings are %v\n", firstNames);
+
+	}
+
+	
+	// var userName string;
+	// var userTickets int;
+	// fmt.Println("Enter your name: ");
+	// fmt.Scan(&userName);
+	// fmt.Println("Enter number of tickets: ");
+    // fmt.Scan(&userTickets);
+	// fmt.Println(&userName);
+	// fmt.Println(&userTickets);
+	// if userTickets > 0{
+	// 	var leftTickets int = remainingTickets - userTickets;
+	// 	fmt.Printf("Booking tickets left to purchase: %v\n", leftTickets); 
+	// };
+
+	
+
+	// fmt.Printf("User %v booked %v tickets\n", userName, userTickets);
 }
