a9cf0993ef090aaae30c5dd37cf73ed7013c4477
Arab45 <horlakunleolayiwola@gmail.com>
2025-06-07 15:58:18 -0700

    refactor booking logic to include user input for booking details and initialize bookings collection

--- booking/booking.go
+++ booking/booking.go
@@ -1,22 +1,47 @@
 package booking
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type BookingUser struct {
 	userId  string
-	product make(map[string]string);
+	product map[string]string;
 	date    time.Time
 	delivery string
 }
 
 func Booking() {
+	var bookings = make([]BookingUser, 0)
 
-	var collections = map[string]BookingUser{
-		userId: userId,
-		product: product,
-		date: date,
-		delivery: delivery
+
+	
+	var userId string
+	var product map[string]string
+	var date time.Time
+	var delivery string
+
+	fmt.Printf("provide your userId:");
+	fmt.Scan(&userId)
+	fmt.Printf("provide your product order:");
+	fmt.Scan(&product)
+	fmt.Printf("current time:");
+	fmt.Scan(&date)
+	fmt.Printf("delivery day:");
+	fmt.Scan(&delivery)
+
+
+
+	var bookingTable = BookingUser{
+		userId:  userId,
+	    product: product,
+	    date: date,
+	    delivery: delivery,	
 	}
+
+	var collection = append(bookings, bookingTable);
+	fmt.Printf("")
 
 
 }
