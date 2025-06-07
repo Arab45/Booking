f29d9ce3216f4fa709650f9c968658007fd883a6
Arab45 <horlakunleolayiwola@gmail.com>
2025-06-07 15:21:14 -0700

    add BookingUser struct and initialize collections for user bookings

--- booking/booking.go
+++ booking/booking.go
@@ -1,5 +1,22 @@
 package booking
 
-func Booking(){
-	
+import "time"
+
+type BookingUser struct {
+	userId  string
+	product make(map[string]string);
+	date    time.Time
+	delivery string
+}
+
+func Booking() {
+
+	var collections = map[string]BookingUser{
+		userId: userId,
+		product: product,
+		date: date,
+		delivery: delivery
+	}
+
+
 }
