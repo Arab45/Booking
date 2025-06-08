1625f877e15d388cc0c42217b06306e451f7c12d
Arab45 <horlakunleolayiwola@gmail.com>
2025-06-08 11:07:44 -0700

    refactor Login function to streamline user input handling and maintain login collection

--- login/login.go
+++ login/login.go
@@ -7,22 +7,25 @@ func Login() {
 		username string
 		password string
 	}
-
-	var collection = make([]Login, 0)
-
-	var username string
-	var password string
-
-	fmt.Printf("insert your username:")
-	fmt.Scan(&username);
-	fmt.Printf("password most be mixture of upperCase and lowerCase:")
-	fmt.Scan(&password);
-
-	var loginDetails = Login{
-		username: username,
-		password: password,
+	
+	var collection []Login
+	
+	for {
+		
+		var username string
+		var password string
+		
+		fmt.Printf("insert your username:")
+		fmt.Scan(&username);
+		fmt.Printf("password most be mixture of upperCase and lowerCase:")
+		fmt.Scan(&password);
+		
+		var loginDetails = Login{
+			username: username,
+			password: password,
+		}
+		
+		collection = append(collection, loginDetails);
+		fmt.Printf("user login collection %v\n", collection);
 	}
-
-	var userTable = append(collection, loginDetails);
-	fmt.Printf("user login collection %v\n", userTable);
 }
